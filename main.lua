cfdebd94bc92f82f3f20e4a6ff4aeaeaba0a3525
PuddlePumpkin <66428305+PuddlePumpkin@users.noreply.github.com>
2025-05-08 21:44:54 -0500

    Update

--- main.lua
+++ main.lua
@@ -1,30 +1,14 @@
-print("[KwaNotificationsLua] Initializing script...")
-
--- Console command handler
-RegisterConsoleCommandHandler("PrintExampleNotification", function(cmd, params, ar)
-    -- Find reference
-    local notif = FindFirstOf("WBP_KwaNotifs_C")
-    if not notif or not notif:IsValid() then
-        print("ERROR: KwaNotifs not found")
-        ar:Log("ERROR: KwaNotifs not found")
-        return true
+print("[KwaNotifs] Initializing logging...")
+local HookCreated
+LoopAsync(3000, function()
+    if HookCreated then return end
+    local ok, err = pcall(function()
+        RegisterHook("/Game/Mods/KwaNotificationsBP/WBP_KwaNotifs.WBP_KwaNotifs_C:Log", function(context, strParam)
+            print("[KwaNotifs] Log: " .. strParam:get():ToString())
+        end)
+    end) 
+    if ok then
+        print("[KwaNotifs] Hook registered successfully")
+        HookCreated = true
     end
-
-    -- Print Notifications
-    notif:Print("Example Notification")
-    notif:PrintDebug("This Won't show up without debug messages being enabled")
-    notif:SetDebugMessagesEnabled(true) -- This enables debug messages, or you can also press ctrl-shift-D
-    notif:PrintDebug("This debug message shows up!")
-
-
-    -- If you want to print notifications with a set length:
-    notif:PrintNotification("Long Notification", 10)
-    notif:PrintNotificationDebug("Long Debug Notification", 10)
-    --you can also print notifications with icons with an Texture2d object reference: 
-    --notif:PrintNotificationWithIcon(String, Duration, TextureObject)
-
-
-    print("Sent notifications...")
-    ar:Log("Sent notifications...")
-    return true
 end)
