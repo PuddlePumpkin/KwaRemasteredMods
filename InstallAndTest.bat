894f5c10c48420350b7e0fe183326e353b3f5780
PuddlePumpkin <66428305+PuddlePumpkin@users.noreply.github.com>
2025-05-14 09:40:39 -0500

    More Prefabs

--- InstallAndTest.bat
+++ InstallAndTest.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo Attempting to end process: OblivionRemastered-Win64-Shipping.exe
+echo Ending Oblivion process if it is open...
 taskkill /f /im OblivionRemastered-Win64-Shipping.exe
 if %errorlevel% equ 0 (
     echo Process ended successfully.
     timeout /t 2
-) else (
-    echo Installing...
 )
+echo:
+echo Installing files...
 
 CALL %~dp0/install.bat
 
@@ -16,6 +16,6 @@ endlocal
 
 "C:\Program Files (x86)\Steam\steam.exe" steam://rungameid/2623190
 
+echo Starting game!...
 timeout /t 3
-
 
